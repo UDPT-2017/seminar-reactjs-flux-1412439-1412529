e4cf89c9336026ec9babf2145031b9974aeb035e
NguyenHoangQuan2503 <nhquan.fit.hcmus@gmail.com>
2017-05-07 13:46:28 +0700

    update demo

--- React_Final/index.js
+++ React_Final/index.js
@@ -1,10 +1,12 @@
 //var Button = require('react-bootstrap').Button;
-var express=require('express');
-var app=express();
+var express = require('express');
+var app = express();
 app.use(express.static("public"))
-app.set("view engine","ejs");
-app.set("views","./views");
-app.listen(3000);
-app.get("/",function(req,res){
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+
+app.listen(process.env.PORT || 3000);
+app.get("/", function (req, res) {
   res.render("trangchu");
 });
